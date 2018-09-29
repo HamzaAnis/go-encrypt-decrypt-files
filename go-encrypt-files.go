6361af742f66c6ef505ac527760b42cc4f4c4c1b
Hamza Anis <hamzaanis9514@gmail.com>
2018-09-29 23:42:10 +0500

    Key input

--- go-encrypt-files.go
+++ go-encrypt-files.go
@@ -1,9 +1,32 @@
 package main
 
-import "github.com/fatih/color"
+import (
+	"bufio"
+	"flag"
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
+)
 
 func main() {
+	c := color.New(color.FgRed, color.Bold)
+	reader := bufio.NewReader(os.Stdin)
+
 	printHeader()
+	key := flag.String("key", "encrypt_files", "key to encrypt and decrypt")
+	flag.Parse()
+	for {
+		if *key == "encrypt_files" {
+			c.Print("please enter the key to encrypt and decrypt files ")
+			key_, err := reader.ReadString('\n')
+			if err != nil {
+				color.Red("error while reading key")
+			}
+			*key = strings.TrimSpace(string(key_))
+		}
+		reader.ReadString('\n')
+	}
 }
 
 func printHeader() {
@@ -26,6 +49,7 @@ func printHeader() {
 		"          ||_______________________________________||\n" +
 		"          |____________________________________[_]__|\n"
 	// banner, _ := ioutil.ReadFile("banner.txt")
+
 	c := color.New(color.FgGreen, color.Bold)
 	c.Println(banner)
 }
